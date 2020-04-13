11bfc059f98d54ee51c2190e1589be97831ccdf8
Maximingesup <34605772+Maximingesup@users.noreply.github.com>
2020-04-13 22:28:50 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
 CREATE USER IF NOT EXISTS 'exporter'@'%' identified BY 'password';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
 GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'max' with grant option;
 FLUSH PRIVILEGES;
